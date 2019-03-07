84a094c46343f8472b6c9f4dabb65e3a80c51047
Justin Campbell <justin@justincampbell.me>
2019-03-07 10:00:40 -0500

    Remove 2nd dep

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
-# With version
 module "security-group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "2.14.0"
-
-  name = "test"
-  vpc_id = "test"
-}
-
-# Without version
-module "security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
 
   name = "test"
   vpc_id = "test"
