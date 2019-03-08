863bf8887fd593b27fb0e23d309e2118bb2120a6
Justin Campbell <justin@justincampbell.me>
2019-03-08 12:08:41 -0500

    3 versions of same dep

--- main.tf
+++ main.tf
@@ -5,3 +5,18 @@ module "security-group" {
   name = "test"
   vpc_id = "test"
 }
+
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "2.13.0"
+
+  name = "test"
+  vpc_id = "test"
+}
+
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+
+  name = "test"
+  vpc_id = "test"
+}
